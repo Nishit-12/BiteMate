a51b55e7df1c5e74d5a7f8a0bece040ff28ae232
Nishit Patel <nishitpatel1227@gmail.com>
2023-10-29 18:34:52 +0530

    css change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Home from "./screens/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./screens/Login";
-import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Signup from "./screens/Signup";
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "./components/ContextReducer";
