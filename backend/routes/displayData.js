764a4f5bca0515faafb495e0fcd2cd652d94cec9
Nishit Patel <136961085+Nishit-12@users.noreply.github.com>
2023-11-10 21:01:26 +0530

    Update displayData.js

--- backend/routes/displayData.js
+++ backend/routes/displayData.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const NodeCache = require("node-cache")
+
+const nodeCache = new NodeCache()
 
 router.post('/foodData',(req,res) => {
+
+    let data;
+
+    if(nodeCache.has("data"))
+    {
+        data = JSON.parse(nodeCache.get("data"))
+    }
+    else
+    {
+        nodeCache.set("data", JSON.stringify([global.food_items,global.food_category]))
+    }
+
     try {
 
-        res.send([global.food_items,global.food_category])
+        res.send(data || [global.food_items,global.food_category])
         
     } catch (error) {
         console.log(error)
